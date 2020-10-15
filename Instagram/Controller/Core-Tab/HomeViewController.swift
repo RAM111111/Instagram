06f29ee23943d7f9080c168e4b558e214b65ef46
RAM111111 <32421153+RAM111111@users.noreply.github.com>
2020-10-15 19:30:25 +0300

    first commit

---
+++ Instagram/Controller/Core-Tab/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by ر on 28/02/1442 AH.
+//  Copyright © 1442 r. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
